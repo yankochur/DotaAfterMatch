4a74251da49e82cc3829561314b18a7bc6b62bd5
Yan Kochur <mr.uanby@mail.ru>
2023-05-03 18:32:02 +0300

    send information about the latest matches into telegram

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import requests
-import telegram
+import configparser
+import telebot
 
+config = configparser.ConfigParser()
+config.read('config.txt')           # make config.txt:
+                                    # [DEFAULT]
+                                    # bot_token={your_token}
+                                    # chat_id={your_chat_id}
 
+bot_token = config.get('DEFAULT', 'bot_token')
+chat_id = config.get('DEFAULT', 'chat_id')
 url = 'https://api.opendota.com/api'
-
+bot = telebot.TeleBot(bot_token)
 
 def main():
     def get_last_matches(account_id):
@@ -12,13 +20,10 @@ def main():
         return last_matches_data
 
     last_matches_info = get_last_matches(371887209)
+
     for last_matches_info_data in last_matches_info:
         print(last_matches_info_data)
+        bot.send_message(chat_id, str(last_matches_info_data), parse_mode='html')
 
-
-def send_info_into_telegram():
-    pass
-
-
-if __name__ == "__main__" :
+if __name__ == "__main__":
     main()
